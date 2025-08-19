44ce9b6c954d80064a981e71a973db9598fd70f4
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-19 20:59:25 +0700

    function with parameters

--- my_printing/src/main.rs
+++ my_printing/src/main.rs
@@ -1,6 +1,14 @@
 fn give_number() -> i32 {
     8
 }
+
+fn multiply(number_one: i32, number_two: i32) {
+    let result = number_one * number_two;
+    println!("{} times {} is {}", number_one, number_two, result);
+}
 fn main() {
-    println!("Hello, world number {} !", give_number());
+    multiply(8, 9);
+    let some_number = 10;
+    let some_other_number = 2;
+    multiply(some_number, some_other_number);
 }
