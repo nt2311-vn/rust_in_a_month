6d1c315477cfee76b9a01504c72e08b975b5bca1
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-21 22:02:02 +0700

    knowing the syntax and common pattern for declare constant or static

--- my_constantandstatic/src/main.rs
+++ my_constantandstatic/src/main.rs
@@ -1,3 +1,10 @@
+const NUMBER_OF_MONTHS: u32 = 12;
+static SEASONS: [&str; 4] = ["Spring", "Summer", "Autumn", "Winter"];
+
+fn print_months() {
+    println!("Number of months in the year: {NUMBER_OF_MONTHS}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_months();
 }
