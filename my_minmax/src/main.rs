820d6cc26c177f1b48478f5d84241127527ff4a7
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-21 13:12:13 +0700

    using MIN or MAX following a type to know the constant smallest or biggest value

--- my_minmax/src/main.rs
+++ my_minmax/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("The smallest i8: {} The biggest i8: {}", i8::MIN, i8::MAX);
+    println!("The smallest u8: {} The biggest u8: {}", u8::MIN, u8::MAX);
+    println!(
+        "The smallest i16: {} The biggest i16: {}",
+        i16::MIN,
+        i16::MAX
+    );
 }
