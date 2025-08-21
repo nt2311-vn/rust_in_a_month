26a9aa1d3c93a07f510f5ffb47f6e8e2ec0fa58c
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-21 22:04:42 +0700

    use references and println

--- my_references/src/main.rs
+++ my_references/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let my_number = 15;
     let single_reference = &my_number;
-    let double_reference = &single_reference;
-    let five_references = &&&&&my_number;
+    let _double_reference = &single_reference;
+    let _five_references = &&&&&my_number;
+
+    let country = String::from("Australia");
+    let ref_one = &country;
+    let _ref_two = &country;
+
+    println!("{}", ref_one);
 }
