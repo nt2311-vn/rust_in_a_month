6b5344d8d0b3afb75c0c6f7bf1f33bce58f03bab
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-21 22:07:52 +0700

    cannot return a reference of variable in a local scope

--- my_references/src/main.rs
+++ my_references/src/main.rs
@@ -1,3 +1,9 @@
+fn return_str() -> &String {
+    let country = String::from("Australia");
+    let country_ref = &country;
+    country_ref
+}
+
 fn main() {
     let my_number = 15;
     let single_reference = &my_number;
@@ -9,4 +15,6 @@ fn main() {
     let _ref_two = &country;
 
     println!("{}", ref_one);
+
+    let country = return_str();
 }
