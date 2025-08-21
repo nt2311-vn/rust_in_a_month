115f23f7ffbda973d6e84f1543e4d3e2ad5aaed9
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-21 13:20:34 +0700

    print all smallest and biggest value from 8 -> 128

--- my_minmax/src/main.rs
+++ my_minmax/src/main.rs
@@ -1,9 +1,51 @@
 fn main() {
-    println!("The smallest i8: {} The biggest i8: {}", i8::MIN, i8::MAX);
-    println!("The smallest u8: {} The biggest u8: {}", u8::MIN, u8::MAX);
+    println!("The smallest i8: {}. The biggest i8: {}", i8::MIN, i8::MAX);
+    println!("The smallest u8: {}. The biggest u8: {}", u8::MIN, u8::MAX);
     println!(
-        "The smallest i16: {} The biggest i16: {}",
+        "The smallest i16: {}. The biggest i16: {}",
         i16::MIN,
         i16::MAX
+    );
+
+    println!(
+        "The smallest u16: {}. The biggest u16 {}",
+        u16::MIN,
+        u16::MAX
+    );
+
+    println!(
+        "The smallest i32: {}. The biggest i32 {}",
+        i32::MIN,
+        i32::MAX
+    );
+
+    println!(
+        "The smallest u32: {}. The biggest u32 {}",
+        u32::MIN,
+        u32::MAX
+    );
+
+    println!(
+        "The smallest i64: {}. The biggest i64 {}",
+        i64::MIN,
+        i64::MAX
+    );
+
+    println!(
+        "The smallest u64: {}. The biggest u64 {}",
+        u64::MIN,
+        u64::MAX
+    );
+
+    println!(
+        "The smallest i128: {}. The biggest i128 {}",
+        i128::MIN,
+        i128::MAX
+    );
+
+    println!(
+        "The smallest u128: {}. The biggest u128 {}",
+        u128::MIN,
+        u128::MAX
     );
 }
