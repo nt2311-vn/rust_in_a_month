c3eabb68dba7e4d155eca04bd73d4d694b3b3224
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-21 20:43:55 +0700

    shadowing x using redeclare or use mut and different types

--- my_shadowing/src/main.rs
+++ my_shadowing/src/main.rs
@@ -1,3 +1,6 @@
+fn times_two(number: i32) -> i32 {
+    number * 2
+}
 fn main() {
     let my_number = 8;
     println!("My number {}", my_number);
@@ -8,4 +11,20 @@ fn main() {
     }
 
     println!("{}", my_number);
+
+    let final_number = {
+        let y = 10;
+        let x = 9;
+        let x = times_two(x);
+        let x = x + y;
+        x
+    };
+
+    println!("The number is now: {}", final_number);
+
+    let x = 9;
+    let mut x = x as f32;
+    x += 0.5;
+
+    println!("The number is now: {}", x);
 }
