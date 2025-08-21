9285290381bba5ae7475927a08ff0c0d35fdecd3
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-21 17:23:51 +0700

    shadowing under a scope of variable

--- my_shadowing/src/main.rs
+++ my_shadowing/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     let my_number = 8;
     println!("My number {}", my_number);
 
-    let my_number = 9.2;
-    println!("My number {}", my_number);
+    {
+        let my_number = 9.2;
+        println!("My number {}", my_number);
+    }
+
+    println!("{}", my_number);
 }
