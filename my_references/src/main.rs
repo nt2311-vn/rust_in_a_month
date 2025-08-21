2efeb67a53b3f1f66c774dedbf5af6b0094e0984
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-21 20:50:17 +0700

    declare single, double and five references

--- my_references/src/main.rs
+++ my_references/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let my_number = 15;
+    let single_reference = &my_number;
+    let double_reference = &single_reference;
+    let five_references = &&&&&my_number;
 }
