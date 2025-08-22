565bb4fc13488105d27523524b9567524f9bc9cd
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-22 17:18:05 +0700

    used clonse method to using memory to copy value on function call of not copytypes

--- my_copytypes/src/main.rs
+++ my_copytypes/src/main.rs
@@ -1,8 +1,17 @@
 fn prints_number(number: i32) {
     println!("{}", number);
 }
+
+fn prints_country(country_name: String) {
+    println!("{}", country_name);
+}
+
 fn main() {
     let my_number = 8;
     prints_number(my_number);
     prints_number(my_number);
+
+    let country = String::from("Kiribati");
+    prints_country(country.clone());
+    prints_country(country);
 }
