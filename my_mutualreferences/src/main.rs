b11e9298ac6753ecc7149085d0655cef51945637
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-22 14:26:14 +0700

    update my mutual references

--- my_mutualreferences/src/main.rs
+++ my_mutualreferences/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let mut my_number = 8;
     let num_ref = &mut my_number;
+
+    *num_ref += 10;
+    println!("{}", my_number);
+
+    let second_number = 800;
+    let tripple_reference = &&&second_number;
+    println!("Are they equal? {}", second_number == ***tripple_reference);
 }
