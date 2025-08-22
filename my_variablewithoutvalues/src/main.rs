39288419286a68b6b2c4fafaffc5760b880bf9da
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-22 22:15:17 +0700

    explore thing when we declare a variable without values'

--- my_variablewithoutvalues/src/main.rs
+++ my_variablewithoutvalues/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let my_variable: i32;
+
+    {
+        let calculation_result = { 57 };
+        my_variable = calculation_result;
+        println!("{my_variable}");
+    }
 }
