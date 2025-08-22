d27b3642e01e24f2e6619d9e420ff34e698b4f3d
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-22 16:01:17 +0700

    mutable reference inside function

--- my_functionwithreference/src/main.rs
+++ my_functionwithreference/src/main.rs
@@ -2,8 +2,16 @@ fn print_country(country_name: &str) {
     println!("{country_name}");
 }
 
+fn add_hungary(country_name: &mut String) {
+    country_name.push_str("-Hungary");
+    println!("Now it says: {country_name}");
+}
+
 fn main() {
     let country = String::from("Austria");
     print_country(&country);
     print_country(&country);
+
+    let mut new_country = String::from("Austria");
+    add_hungary(&mut new_country);
 }
