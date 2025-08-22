c431315af7b83224e53deec827e55e103cba6eb4
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-22 17:37:12 +0700

    using clone on string every time in a loop ends up having n clones

--- my_copytypes/src/main.rs
+++ my_copytypes/src/main.rs
@@ -6,6 +6,10 @@ fn prints_country(country_name: String) {
     println!("{}", country_name);
 }
 
+fn get_length(input: String) {
+    println!("It's {} words long", input.split_whitespace().count());
+}
+
 fn main() {
     let my_number = 8;
     prints_number(my_number);
@@ -14,4 +18,11 @@ fn main() {
     let country = String::from("Kiribati");
     prints_country(country.clone());
     prints_country(country);
+
+    let mut my_string = String::new();
+
+    for _ in 0..50 {
+        my_string.push_str("Here are some more words ");
+        get_length(my_string.clone());
+    }
 }
