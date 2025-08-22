76c17ed9dc1636ac310c28cf9878f04e3b954f38
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-22 14:40:11 +0700

    explain shadwoing dont destroy value it just block

--- my_shadowingagain/src/main.rs
+++ my_shadowingagain/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let country = String::from("Austria");
+    let country_ref = &country;
+    let country = 8;
+
+    println!("{country_ref} {country}")
 }
