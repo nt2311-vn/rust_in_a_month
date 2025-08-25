7ffc57238f55b763da29fb43c2ef8083b77c0526
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-25 13:46:14 +0700

    explore slice a piece of array

--- my_array/src/main.rs
+++ my_array/src/main.rs
@@ -8,4 +8,19 @@ fn main() {
 
     let my_numbers = [0, 10, -20];
     println!("{}", my_numbers[1]);
+
+    let array_of_ten = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let two_to_five = &array_of_ten[2..5];
+    let start_at_one = &array_of_ten[1..];
+    let end_at_five = &array_of_ten[..5];
+    let everything = &array_of_ten[..];
+
+    println!(
+        "Two to five: {two_to_five:?},
+Start at one: {start_at_one:?},
+End at five : {end_at_five:?},
+Everything: {everything:?}
+"
+    );
 }
