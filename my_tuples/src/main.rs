ed9705eeeaf638a4883251905270ac9062f2f51d
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-26 11:48:37 +0700

    access value inside tuple by using . with index

--- my_tuples/src/main.rs
+++ my_tuples/src/main.rs
@@ -1,3 +1,23 @@
+fn just_makes_an_i32() {
+    let _unused_number = 10;
+}
 fn main() {
-    println!("Hello, world!");
+    just_makes_an_i32();
+
+    let random_tuple = ("Here is a name", 8, vec!['a'], 'b', [8, 9, 10], 7.7);
+
+    println!(
+        "Inside the tuple is: First item: {:?}
+Second item: {:?}
+Third item: {:?}
+Fourth item: {:?}
+Fifth item: {:?}
+Sixth item: {:?}",
+        random_tuple.0,
+        random_tuple.1,
+        random_tuple.2,
+        random_tuple.3,
+        random_tuple.4,
+        random_tuple.5
+    )
 }
