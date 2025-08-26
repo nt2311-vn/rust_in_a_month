58db6876f217934a2ae09e30f589229906f97180
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-26 10:02:37 +0700

    explore how to make a vector and some method to add element and get index, get slice from vector

---
+++ my_vector/src/main.rs
@@ -0,0 +1,28 @@
+use std::vec;
+
+fn main() {
+    let name1 = String::from("Windy");
+    let name2 = String::from("Gomesy");
+
+    let mut my_vec: Vec<String> = Vec::new();
+    my_vec.push(name1);
+    my_vec.push(name2);
+
+    println!("My vec: {:?}", my_vec);
+    let other_vec = vec![8, 10, 10];
+    println!("Other vec: {:?}", other_vec);
+
+    let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let three_of_five = &vec_of_ten[2..5];
+    let start_at_two = &vec_of_ten[1..];
+    let end_at_five = &vec_of_ten[..5];
+    let everything = &vec_of_ten[..];
+
+    println!(
+        "Three of five: {:?}, 
+start at two: {:?}
+end at five: {:?}
+everything: {:?}",
+        three_of_five, start_at_two, end_at_five, everything
+    )
+}
