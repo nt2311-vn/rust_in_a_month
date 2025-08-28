3bb8db083f7a38d87c748bd6f85285d619cde817
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-28 10:43:04 +0700

    using more conditional in if else branc

--- my_controlflow/src/main.rs
+++ my_controlflow/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let my_number = 5;
-    if my_number == 7 {
-        println!("It's seven");
+    if my_number % 2 == 1 && my_number > 0 {
+        println!("It's a positive odd number");
     } else if my_number == 6 {
         println!("It's six")
     } else {
