63ef1b3bdf229b9af879829c96d65dc4cd8653e7
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-28 11:52:19 +0700

    match with arm with if statement

--- my_controlflow/src/main.rs
+++ my_controlflow/src/main.rs
@@ -34,4 +34,15 @@ fn main() {
         ("cloudy", "warm") => println!("It's dark but not bad"),
         _ => println!("Not sure what the waether is."),
     }
+
+    let children = 5;
+    let married = true;
+
+    match (children, married) {
+        (children, married) if married == false => println!("Not married with {children} kids"),
+        (children, married) if children == 0 && married == true => {
+            println!("Married but no children")
+        }
+        _ => println!("Married? {married}. Number of children: {children}."),
+    }
 }
