c706dee73d33b1177229a27ce769e85b6a689b8c
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-28 11:39:08 +0700

    match arm with value as tuples

--- my_controlflow/src/main.rs
+++ my_controlflow/src/main.rs
@@ -24,4 +24,14 @@ fn main() {
     };
 
     println!("Second number {second_number}");
+
+    let sky = "cloudy";
+    let temperature = "warm";
+
+    match (sky, temperature) {
+        ("cloudy", "cold") => println!("It's dark and unpleasant today"),
+        ("clear", "warm") => println!("It's a nice day"),
+        ("cloudy", "warm") => println!("It's dark but not bad"),
+        _ => println!("Not sure what the waether is."),
+    }
 }
