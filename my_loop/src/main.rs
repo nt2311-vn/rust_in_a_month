ac9eadacf3b8e2f3f133270a996e10f194b7faca
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-28 16:05:13 +0700

    label loop and escape early

--- my_loop/src/main.rs
+++ my_loop/src/main.rs
@@ -7,4 +7,27 @@ fn main() {
             break;
         }
     }
+
+    let mut other_counter = 0;
+    let mut other_counter2 = 0;
+
+    println!("Now entering the first loop.");
+
+    'first_loop: loop {
+        other_counter += 1;
+        println!("The other counter is now: {}", other_counter);
+
+        if other_counter > 5 {
+            println!("Now entering the second loop.");
+
+            'second_loop: loop {
+                println!("The second counter is now: {}", other_counter2);
+                other_counter2 += 1;
+
+                if other_counter2 == 3 {
+                    break 'first_loop;
+                }
+            }
+        }
+    }
 }
