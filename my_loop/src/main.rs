7b43bfd269a199f4954c72cde19baa62d84a3823
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-29 13:16:40 +0700

    use _ to ignore capture variable in loop

--- my_loop/src/main.rs
+++ my_loop/src/main.rs
@@ -45,4 +45,8 @@ fn main() {
     for number in 0..=3 {
         println!("The next number is: {}", number);
     }
+
+    for _ in 0..3 {
+        println!("Printing the same thing three times");
+    }
 }
