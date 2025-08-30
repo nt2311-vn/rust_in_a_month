262f3784a0b2e8b5f3cf036f2db2bee9b987f4c3
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-30 10:10:29 +0700

    explore name struct

--- my_struct/src/main.rs
+++ my_struct/src/main.rs
@@ -1,7 +1,32 @@
 struct FileDirectory;
 struct ColorRgb(u8, u8, u8);
+struct SizeAndColor {
+    size: u32,
+    color: ColorRgb,
+}
+
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String,
+}
 
 fn main() {
     let my_color = ColorRgb(50, 0, 50);
     println!("The second part of the color is: {}", my_color.1);
+
+    let size_and_color = SizeAndColor {
+        size: 150,
+        color: my_color,
+    };
+
+    let population = 500_000;
+    let capital = String::from("Elista");
+    let leader_name = String::from("Batu Khasikov");
+
+    let kalmykia = Country {
+        population,
+        capital,
+        leader_name,
+    };
 }
