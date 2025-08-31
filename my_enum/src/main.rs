e905677e71086accfbcbd16a0b6dd559191ecde3
nt2311-vn <nguyentoan231196@gmail.com>
2025-08-31 21:57:02 +0700

    declare enum and add derive debug print to struct and enum, println

--- my_enum/src/main.rs
+++ my_enum/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+enum Climate {
+    Tropical,
+    Dry,
+    Temperate,
+    Continential,
+    Polar,
+}
+
+#[derive(Debug)]
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String,
+    climate: Climate,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let kalmykia = Country {
+        population: 500_000,
+        capital: String::from("Elista"),
+        leader_name: String::from("Batu Khasikov"),
+        climate: Climate::Continential,
+    };
+
+    println!("The climate is {:#?}", kalmykia);
 }
