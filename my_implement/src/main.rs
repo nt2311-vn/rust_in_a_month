dac5f71a6b66c762c8327fa106af3dbe0ee65357
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-01 21:22:04 +0700

    implement enum and structs and implement block of method with struct

--- my_implement/src/main.rs
+++ my_implement/src/main.rs
@@ -1,3 +1,46 @@
+#[derive(Debug)]
+enum AnimalType {
+    Cat,
+    Dog,
+}
+
+#[derive(Debug)]
+struct Animal {
+    age: u8,
+    animal_type: AnimalType,
+}
+
+impl Animal {
+    fn new_cat() -> Self {
+        Self {
+            age: 10,
+            animal_type: AnimalType::Cat,
+        }
+    }
+
+    fn check_type(&self) {
+        match self.animal_type {
+            AnimalType::Cat => println!("The animal is a cat."),
+            AnimalType::Dog => println!("The animal is a dog."),
+        }
+    }
+
+    fn change_to_dog(&mut self) {
+        self.animal_type = AnimalType::Dog;
+        println!("Changed animal to dog! Now it's a {self:#?}");
+    }
+
+    fn change_to_cat(&mut self) {
+        self.animal_type = AnimalType::Cat;
+        println!("Changed animal to cat! Now it's a {self:#?}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut new_animal = Animal::new_cat();
+    new_animal.check_type();
+    new_animal.change_to_dog();
+    new_animal.check_type();
+    new_animal.change_to_cat();
+    new_animal.check_type();
 }
