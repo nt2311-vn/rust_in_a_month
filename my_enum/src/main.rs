e8778928f749ce60529211a8bb47219d74f06109
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-01 12:46:09 +0700

    use keyword to not type enum :: everytimes

--- my_enum/src/main.rs
+++ my_enum/src/main.rs
@@ -35,6 +35,27 @@ fn check_skystate(state: &ThingsInTheSky) {
     }
 }
 
+#[derive(Debug)]
+enum Mood {
+    Happy,
+    Sleepy,
+    NotBad,
+    Angry,
+}
+
+fn match_mood(mood: &Mood) -> i32 {
+    use Mood::*;
+
+    let happiness_level = match mood {
+        Happy => 10,
+        Sleepy => 6,
+        NotBad => 7,
+        Angry => 2,
+    };
+
+    happiness_level
+}
+
 fn main() {
     let kalmykia = Country {
         population: 500_000,
@@ -48,4 +69,8 @@ fn main() {
     let time = 8;
     let skytate = create_skystate(time);
     check_skystate(&skytate);
+
+    let my_mood = Mood::Happy;
+    let happiness_level = match_mood(&my_mood);
+    println!("Out of 1 to 10, my happiness is {happiness_level}");
 }
