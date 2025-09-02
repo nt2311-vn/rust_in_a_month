75257c4177f9f6053b140384985faa80e00a8089
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-02 09:57:25 +0700

    use destructure to get field value inside of struct

--- my_destructuring/src/main.rs
+++ my_destructuring/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    real_name: String,
+    height: u8,
+    happiness: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let papa_doc = Person {
+        name: "Papa Doc".to_string(),
+        real_name: "Clarence".to_string(),
+        height: 170,
+        happiness: false,
+    };
+
+    let Person {
+        name,
+        real_name,
+        height,
+        happiness,
+    } = papa_doc;
+    println!("They call him {name} but his real name is {real_name}. He is {height} cm tall and is he happy? {happiness}")
 }
