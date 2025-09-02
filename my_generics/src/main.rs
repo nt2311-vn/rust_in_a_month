0587e2ebc071ecf15887cb473828631c6925f3e8
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-02 20:55:18 +0700

    we can force the generic type have some trait Debug as a promise what we provide

--- my_generics/src/main.rs
+++ my_generics/src/main.rs
@@ -1,9 +1,16 @@
+use std::fmt::Debug;
+
 fn return_item<T>(item: T) -> T {
     println!("Here is your item.");
     item
 }
 
+fn print_item<T: Debug>(item: T) {
+    println!("Here is your item: {item:?}");
+}
+
 fn main() {
     let item = return_item(5);
     println!("Item {item}");
+    print_item(5);
 }
