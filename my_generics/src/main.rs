a7ca8118236e5dc9fed1e0e1eb007725ada4ca83
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-02 20:57:41 +0700

    generic can work with any type struct, enum is included

--- my_generics/src/main.rs
+++ my_generics/src/main.rs
@@ -1,5 +1,11 @@
 use std::fmt::Debug;
 
+#[derive(Debug)]
+struct Animal {
+    name: String,
+    age: u8,
+}
+
 fn return_item<T>(item: T) -> T {
     println!("Here is your item.");
     item
@@ -13,4 +19,13 @@ fn main() {
     let item = return_item(5);
     println!("Item {item}");
     print_item(5);
+
+    let charlie = Animal {
+        name: "Charlie".to_string(),
+        age: 1,
+    };
+
+    let number = 55;
+    print_item(number);
+    print_item(charlie);
 }
