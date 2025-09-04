0db63c93df0a516fa507c135214604b80f15b45f
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-04 11:56:24 +0700

    fix some error panic with Option wrapped in the value and run project

--- my_option/src/main.rs
+++ my_option/src/main.rs
@@ -1,3 +1,12 @@
+fn try_take_fifth(value: Vec<i32>) -> Option<i32> {
+    if value.len() < 5 {
+        None
+    } else {
+        Some(value[4])
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let small = vec![1, 2];
+    let big = vec![1, 2, 3, 4, 5];
+    println!("{:?}, {:?}", try_take_fifth(small), try_take_fifth(big));
 }
