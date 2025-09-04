8b2cec813f2560d38daefd5ff013a626947d9a5c
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-04 13:25:12 +0700

    handle option use match to unwrap value of Option

--- my_option/src/main.rs
+++ my_option/src/main.rs
@@ -5,12 +5,27 @@ fn try_take_fifth(value: Vec<i32>) -> Option<i32> {
         Some(value[4])
     }
 }
+
+fn handle_options(my_option: &Vec<Option<i32>>) {
+    for item in my_option {
+        match item {
+            Some(number) => println!("Found a {number}!"),
+            None => println!("Found a None!"),
+        }
+    }
+}
+
 fn main() {
     let small = vec![1, 2];
     let big = vec![1, 2, 3, 4, 5];
-    println!(
-        "{:?}, {:?}",
-        try_take_fifth(small).unwrap(),
-        try_take_fifth(big).unwrap()
-    );
+    // println!(
+    //     "{:?}, {:?}",
+    //     try_take_fifth(small).unwrap(),
+    //     try_take_fifth(big).unwrap()
+    // );
+
+    let mut option_vec = Vec::new();
+    option_vec.push(try_take_fifth(small));
+    option_vec.push(try_take_fifth(big));
+    handle_options(&option_vec);
 }
