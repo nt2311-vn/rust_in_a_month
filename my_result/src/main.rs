a5f9d3341e2d760dfde267195bf57fb0c8f31ca4
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-04 17:32:06 +0700

    use is_ok(), is_error() to check Result value Ok(), Err()

--- my_result/src/main.rs
+++ my_result/src/main.rs
@@ -2,6 +2,20 @@ fn check_error() -> Result<(), ()> {
     Ok(())
 }
 
+fn see_if_number_is_even(input: i32) -> Result<(), ()> {
+    if input % 2 == 0 {
+        return Ok(());
+    } else {
+        return Err(());
+    }
+}
+
 fn main() {
     let _ = check_error();
+
+    if see_if_number_is_even(5).is_ok() {
+        println!("It's okay, guys");
+    } else {
+        println!("It's an error, guys");
+    }
 }
