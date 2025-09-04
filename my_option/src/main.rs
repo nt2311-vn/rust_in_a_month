93d9d8215d764fe2f2286b74af67fad16382f3d2
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-04 11:58:01 +0700

    we can get value inside option some<value> with unwrap() method but to be sure unwrap cannot use with none value

--- my_option/src/main.rs
+++ my_option/src/main.rs
@@ -8,5 +8,9 @@ fn try_take_fifth(value: Vec<i32>) -> Option<i32> {
 fn main() {
     let small = vec![1, 2];
     let big = vec![1, 2, 3, 4, 5];
-    println!("{:?}, {:?}", try_take_fifth(small), try_take_fifth(big));
+    println!(
+        "{:?}, {:?}",
+        try_take_fifth(small).unwrap(),
+        try_take_fifth(big).unwrap()
+    );
 }
