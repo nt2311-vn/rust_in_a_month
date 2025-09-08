4dba0ace142734e46dbfd99be5b8f587af2c29d5
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-08 15:26:12 +0700

    hash map collection in rust

--- my_collection/src/main.rs
+++ my_collection/src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct City {
+    name: String,
+    population: HashMap<i32, i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tallinn = City {
+        name: "Tallinn".to_string(),
+        population: HashMap::new(),
+    };
+
+    tallinn.population.insert(2020, 437_619);
+    tallinn.population.insert(1372, 3_250);
+    tallinn.population.insert(1851, 24_000);
+
+    for (year, population) in tallinn.population {
+        println!("In {year}, Tallinn had a population of {population}");
+    }
 }
