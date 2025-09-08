58fa8f65f0a5f6dac2c1dc74fa27424dd7de11a0
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-08 14:35:36 +0700

    run project with range from 0 to 9 to get element index in vector

--- my_patternmatching/src/main.rs
+++ my_patternmatching/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let my_vec = vec![2, 3, 4];
-    let get_one = my_vec.get(0);
-    let get_two = my_vec.get(10);
-    println!("{:?}", get_one);
-    println!("{:?}", get_two);
+
+    for index in 0..10 {
+        match my_vec.get(index) {
+            Some(number) => println!("The number is: {number}"),
+            None => {}
+        }
+    }
 }
