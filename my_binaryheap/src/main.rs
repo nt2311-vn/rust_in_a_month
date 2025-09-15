eacde9589bab60af453fda7d07f1e172780865c2
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-09-15 20:37:06 +0700

    binary_heap is good case using in to make to do list withh some priority job

--- my_binaryheap/src/main.rs
+++ my_binaryheap/src/main.rs
@@ -14,4 +14,16 @@ fn main() {
     while let Some(num) = binary_heap.pop() {
         println!("Popped off {num}. Remaining numbers are: {binary_heap:?}");
     }
+
+    let mut jobs = BinaryHeap::new();
+
+    jobs.push((100, "Reply to email from the CEO"));
+    jobs.push((80, "Finish the report today"));
+    jobs.push((10, "Watch some Youtube"));
+    jobs.push((70, "Tell your team members thanks for always working_hard"));
+    jobs.push((30, "Plan whho to hire next for the team"));
+
+    for (_, job) in jobs {
+        println!("You need to do: {job}");
+    }
 }
