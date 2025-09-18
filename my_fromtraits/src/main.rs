b525e67d291940e74648906d3a4664584f070e5b
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-18 16:15:21 +0700

    create manual from traits for different structs

--- my_fromtraits/src/main.rs
+++ my_fromtraits/src/main.rs
@@ -1,3 +1,37 @@
+#[derive(Debug)]
+struct City {
+    name: String,
+    population: u32,
+}
+
+impl City {
+    fn new(name: &str, population: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            population,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Country {
+    cities: Vec<City>,
+}
+
+impl From<Vec<City>> for Country {
+    fn from(cities: Vec<City>) -> Self {
+        Self { cities }
+    }
+}
+
+impl Country {
+    fn print_cities(&self) {
+        for city in &self.cities {
+            println!("{:?} has population of {:?}.", city.name, city.population);
+        }
+    }
+}
+
 fn main() {
     let array_vec = Vec::from([8, 9, 10]);
     println!("Vec from array: {array_vec:?}");
@@ -7,4 +41,11 @@ fn main() {
 
     let string_vec = Vec::from("What will a String be?".to_string());
     println!("Vec from string: {string_vec:?}");
+
+    let helsinki = City::new("Helsinki", 631_695);
+    let turku = City::new("Turku", 186_756);
+
+    let finland_cities = vec![helsinki, turku];
+    let findland = Country::from(finland_cities);
+    findland.print_cities();
 }
