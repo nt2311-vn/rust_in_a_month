2b6202c99b3a70832ca4733b9e50042ba04b443a
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-18 11:30:31 +0700

    defint two struct and one trait to implement for both

--- my_traits/src/main.rs
+++ my_traits/src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+struct Dog {
+    name: String,
+}
+
+struct Parrot {
+    name: String,
+}
+
+trait DogLike {
+    fn bark(&self) {
+        println!("Woof woof!");
+    }
+
+    fn run(&self) {
+        println!("The dog is running!");
+    }
+}
+
+impl DogLike for Dog {}
+impl DogLike for Parrot {}
+
+fn main() {
+    let rover = Dog {
+        name: "Rover".to_string(),
+    };
+
+    let brian = Parrot {
+        name: "Brian".to_string(),
+    };
+
+    rover.bark();
+    rover.run();
+    brian.bark();
+}
