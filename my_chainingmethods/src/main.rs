cb61ddd57499cd69703b16a730d82773083c8a16
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-22 16:27:10 +0700

    write imperative way to create a vector from 1 to 10 using a initialte empty collection

--- my_chainingmethods/src/main.rs
+++ my_chainingmethods/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut new_vec = Vec::new();
+    let mut counter = 1;
+
+    loop {
+        new_vec.push(counter);
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    }
+
+    println!("{new_vec:?}");
 }
