fd88931eb5b74409e222e6db7ee8e98bb05c25ac
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-22 16:16:07 +0700

    using asref as a low cost of implementation traits of type to its references

--- my_asref/src/main.rs
+++ my_asref/src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn print_it<T: AsRef<str>>(input: T) {
+    println!("{}", input.as_ref());
+}
+
+fn main() {
+    print_it("Please print me");
+    print_it("Also, please print me".to_string());
+}
