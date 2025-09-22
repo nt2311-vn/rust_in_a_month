dc0a4adf21b9ff65dd9a9cf733037d716f46ca2a
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-22 16:53:36 +0700

    update

--- my_chainingmethods/src/main.rs
+++ my_chainingmethods/src/main.rs
@@ -14,4 +14,9 @@ fn main() {
 
     let functional_vec = (1..).take(9).collect::<Vec<i32>>();
     println!("{functional_vec:?}");
+
+    let fixed_vec = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let collect_vec = fixed_vec.into_iter().skip(3).take(4).collect::<Vec<i32>>();
+    println!("{collect_vec:?}");
 }
