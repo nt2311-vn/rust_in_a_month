212e9ac46bc3e3146d7b571f5a8c79c94e60c7f5
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-22 15:47:42 +0700

    implement fmt and derive clone debug for struct file

--- orphan_rule/src/main.rs
+++ orphan_rule/src/main.rs
@@ -1,8 +1,19 @@
+#[derive(Clone, Debug)]
 struct File(String);
+
+impl std::fmt::Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let as_bytes = format!("{:?}", self.0.as_bytes());
+        write!(f, "{as_bytes}")
+    }
+}
 
 fn main() {
     let my_file = File(String::from("I am a file contents"));
     let my_string = String::from("I am a file contents");
 
+    let file = File(String::from("I am file contents"));
     println!("{}", my_file.0 == my_string);
+    println!("{file:?}");
+    println!("{file}");
 }
