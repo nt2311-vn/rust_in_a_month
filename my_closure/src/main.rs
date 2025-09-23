f26e964d77c8adf9d3836dd5208cae156feeb01e
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 13:53:34 +0700

    when multiple line using bracket

--- my_closure/src/main.rs
+++ my_closure/src/main.rs
@@ -2,4 +2,13 @@ fn main() {
     let my_closure = |x: i32| println!("{x}");
     my_closure(5);
     my_closure(5 + 5);
+
+    let other_closure = || {
+        let number = 7;
+        let other_number = 10;
+
+        println!("The two numbers are {number} and {other_number}.");
+    };
+
+    other_closure();
 }
