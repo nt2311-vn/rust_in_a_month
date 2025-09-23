df81ffe7ff93372a6448ec2f299ae6c400318c50
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 15:27:05 +0700

    doing map without collect we spread the structure of iteration and do nothing with it

--- my_lazyclosure/src/main.rs
+++ my_lazyclosure/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
 
     let double_vec: Vec<i32> = num_vec.iter().map(|num| num * 2).collect();
     println!("{:?}", double_vec);
+
+    num_vec
+        .iter()
+        .enumerate()
+        .map(|(index, num)| format!("Index {index} is {num}"));
 }
