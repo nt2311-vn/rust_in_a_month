d8cad674696cb775e45b2a5437907e1b945beaf4
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 14:39:38 +0700

    build project understand closure inside an iterator

--- my_closureinsideiterator/src/main.rs
+++ my_closureinsideiterator/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    (1..=3).for_each(|num| println!("{num}"));
+    (1..=3).for_each(|num| {
+        println!("Got a {num}!");
+
+        if num % 2 == 0 {
+            println!("It's even")
+        } else {
+            println!("It's odd")
+        };
+    });
 }
