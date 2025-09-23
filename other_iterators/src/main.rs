50a7d8e6e50b5d64cbad7ba7b839601a77e13445
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 13:27:05 +0700

    other iterators create struct library and add traits into struct

--- other_iterators/src/main.rs
+++ other_iterators/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+#[derive(Debug)]
+struct Library {
+    name: String,
+    books: Vec<String>,
+}
+
+impl Library {
+    fn add_book(&mut self, book: &str) {
+        self.books.push(book.to_string());
+    }
+
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            books: Vec::new(),
+        }
+    }
+}
+fn main() {
+    let my_library = Library::new("Calgary");
+    println!("{my_library:?}");
+}
