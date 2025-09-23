058f3abf0d959c3b61d65c0897f045c741448749
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 11:40:53 +0700

    explore iter_mut and into_each

--- my_iterators/src/main.rs
+++ my_iterators/src/main.rs
@@ -17,4 +17,20 @@ fn main() {
     }
 
     println!("{vector2:?}");
+
+    let another_vector1 = vec![1, 2, 3];
+
+    let another_vector1_a = another_vector1.iter().map(|x| x * 10).collect::<Vec<i32>>();
+
+    let another_vector1_b = another_vector1
+        .into_iter()
+        .map(|x| x * 10)
+        .collect::<Vec<i32>>();
+
+    let mut another_vector2 = vec![10, 20, 30];
+    another_vector2.iter_mut().for_each(|x| *x += 100);
+
+    println!("{:?}", another_vector1_a);
+    println!("{:?}", another_vector1_b);
+    println!("{:?}", another_vector2);
 }
