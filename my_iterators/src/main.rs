79d002091629dca0fd33a3c93df5b29fd74159b9
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 11:30:52 +0700

    explore iterator and iterator_mut()

---
+++ my_iterators/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let vector1 = vec![1, 2, 3];
+
+    let mut vector2 = vec![10, 20, 30];
+
+    for num in vector1.iter() {
+        println!("Printing a &i32: {num}");
+    }
+
+    for num in vector1 {
+        println!("Printing an i32: {num}");
+    }
+
+    for num in vector2.iter_mut() {
+        *num *= 10;
+        println!("num is now {num}");
+    }
+
+    println!("{vector2:?}");
+}
