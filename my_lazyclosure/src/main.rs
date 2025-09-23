dbb46e7acbc0e630445882c49a03c921aa153dea
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 15:22:08 +0700

    expolre the lazy feature of closure

--- my_lazyclosure/src/main.rs
+++ my_lazyclosure/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let num_vec = vec![2, 4, 6];
+
+    let double_vec: Vec<i32> = num_vec.iter().map(|num| num * 2).collect();
+    println!("{:?}", double_vec);
 }
