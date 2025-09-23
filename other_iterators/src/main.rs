66c30fe980eca06a5354d6d9c55a13b20963e602
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-23 13:43:09 +0700

    implement Iterator method for BookLibrary

--- other_iterators/src/main.rs
+++ other_iterators/src/main.rs
@@ -1,22 +1,55 @@
 #[derive(Debug)]
 struct Library {
     name: String,
-    books: Vec<String>,
+    books: BookCollection,
 }
+
+#[derive(Debug, Clone)]
+struct BookCollection(Vec<String>);
 
 impl Library {
     fn add_book(&mut self, book: &str) {
-        self.books.push(book.to_string());
+        self.books.0.push(book.to_string());
     }
 
     fn new(name: &str) -> Self {
         Self {
             name: name.to_string(),
-            books: Vec::new(),
+            books: BookCollection(Vec::new()),
+        }
+    }
+
+    fn get_books(&self) -> BookCollection {
+        self.books.clone()
+    }
+}
+
+impl Iterator for BookCollection {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        match self.0.pop() {
+            Some(book) => {
+                println!("Accessing book: {book}");
+                Some(book)
+            }
+
+            None => {
+                println!("Out of books at the library!");
+                None
+            }
         }
     }
 }
+
 fn main() {
-    let my_library = Library::new("Calgary");
-    println!("{my_library:?}");
+    let mut my_library = Library::new("Calgary");
+    my_library.add_book("The Doom of the Darksword");
+    my_library.add_book("Demian - die Geschichte einer Jugend");
+    my_library.add_book("구운몽");
+    my_library.add_book("吾輩は猫である");
+
+    for item in my_library.get_books() {
+        println!("{item}");
+    }
 }
