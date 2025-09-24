be266c8ed1b8deeab6cf34a535085e5a864aee4f
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-24 15:09:28 +0700

    update build target

--- my_moreoperator/src/main.rs
+++ my_moreoperator/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let num_array = ["8", "9", "Hi", "9898989898"];
+
+    let mut char_vec = vec![];
+
+    for index in 0..5 {
+        char_vec.push(
+            num_array
+                .get(index)
+                .and_then(|number| number.parse::<u32>().ok())
+                .and_then(|number| char::try_from(number).ok()),
+        );
+    }
+
+    println!("{:?}", char_vec)
 }
