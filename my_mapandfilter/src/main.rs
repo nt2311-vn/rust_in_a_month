75f9d31852e8e9aff42c5756666c84f186737bb7
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-24 11:46:27 +0700

    filter map example when parse an string into float using .ok() to turn and Result into Options

--- my_mapandfilter/src/main.rs
+++ my_mapandfilter/src/main.rs
@@ -58,4 +58,13 @@ fn main() {
         .collect::<Vec<String>>();
 
     println!("{:?}", all_the_ceos);
+
+    let user_input = vec!["8.9", "Nine point to five", "8.0", "7.6", "eleventy-twelve"];
+
+    let successful_numbers = user_input
+        .iter()
+        .filter_map(|input| input.parse::<f32>().ok())
+        .collect::<Vec<f32>>();
+
+    println!("{:?}", successful_numbers);
 }
