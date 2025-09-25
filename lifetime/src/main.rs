5fe51105583e209fabddb39c43feab7e6b89a0cf
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-25 09:30:28 +0700

    try to declare a struct field as static reference but its references does not live long for the whole programming

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+struct City {
+    name: &'static str,
+    date_founded: u32,
+}
+
 fn works() -> &'static str {
     "I live forever!"
 }
@@ -9,4 +15,13 @@ fn returns_str() -> &'static str {
 fn main() {
     let my_str = returns_str();
     println!("{my_str}");
+
+    let city_names = vec!["Ichinomiya".to_string(), "Kurume".to_string()];
+
+    let my_city = City {
+        name: &city_names[0],
+        date_founded: 1921,
+    };
+
+    println!("{} was founded in {}", my_city.name, my_city.date_founded);
 }
