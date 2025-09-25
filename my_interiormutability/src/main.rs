5fb6d813638a58de7661c6e9f7d04809a391e306
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-25 14:15:37 +0700

    create a struct PhoneModel

--- my_interiormutability/src/main.rs
+++ my_interiormutability/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+struct PhoneModel {
+    company_name: String,
+    model_name: String,
+    screen_size: f32,
+    memory: usize,
+    date_issued: u32,
+    on_sale: bool,
 }
+
+fn main() {}
