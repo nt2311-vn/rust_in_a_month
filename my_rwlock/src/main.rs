d8610ef9bf5bc90991eebcddaabcf5acd5d21b83
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 13:18:57 +0700

    using try_write() to get info of in casing cannot access to the rwlock

--- my_rwlock/src/main.rs
+++ my_rwlock/src/main.rs
@@ -6,12 +6,19 @@ fn main() {
     let read1 = my_rwlock.read().unwrap();
     let read2 = my_rwlock.read().unwrap();
 
-    println!("{read1:?}, {read2:?}");
-    drop(read1);
-    drop(read2);
+    // println!("{read1:?}, {read2:?}");
+    // drop(read1);
+    // drop(read2);
+    //
+    // let mut write1 = my_rwlock.write().unwrap();
+    // *write1 = 6;
+    // drop(write1);
+    // println!("{:?}", my_rwlock);
 
-    let mut write1 = my_rwlock.write().unwrap();
-    *write1 = 6;
-    drop(write1);
-    println!("{:?}", my_rwlock);
+    if let Ok(mut number) = my_rwlock.try_write() {
+        *number += 10;
+        println!("Now the number is {}", number);
+    } else {
+        println!("Couldn't get write access, sorry!");
+    }
 }
