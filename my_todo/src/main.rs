0501a8f2fdb499cd52daaf5dc0a52aeeba27965e
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 14:15:30 +0700

    use to_do for easiser to make a function sastisfy its signature provided that all the variable and type is already declared

--- my_todo/src/main.rs
+++ my_todo/src/main.rs
@@ -1,3 +1,24 @@
+struct Book;
+
+enum BookType {
+    HardCover,
+    SoftCover,
+}
+
+fn get_book(book: &Book) -> Option<String> {
+    todo!();
+}
+fn delete_book(book: &Book) -> Result<(), String> {
+    todo!();
+}
+fn check_book_type(book_type: &BookType) {
+    match book_type {
+        BookType::HardCover => println!("It's hardcover"),
+        BookType::SoftCover => println!("It's softcover"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let book_type = BookType::HardCover;
+    check_book_type(&book_type);
 }
