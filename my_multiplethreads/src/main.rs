25454bda32140259d1cd1b9904e442c5c0e7789f
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 13:43:31 +0700

    importing and rename inside funciton

--- my_multiplethreads/src/main.rs
+++ my_multiplethreads/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::cell::{Cell, RefCell};
+
+enum MapDirection {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
 }
+
+fn give_direction(direction: &MapDirection) {
+    use MapDirection::*;
+    match direction {
+        North => println!("You are heading north."),
+        NorthEast => println!("You are heading northeast."),
+        East => println!("You are heading east."),
+        SouthEast => println!("You are heading south east"),
+        South => println!("You are heading south"),
+        SouthWest => println!("You are heading south west"),
+        West => println!("You are heading west"),
+        NorthWest => println!("You are heading north west"),
+    }
+}
+fn main() {}
