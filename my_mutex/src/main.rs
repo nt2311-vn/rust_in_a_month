53c0dfbea270eb8124c582062d3718fd8deb802d
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 10:26:01 +0700

    a mutex is unlock when its mutex changer is out of scope

--- my_mutex/src/main.rs
+++ my_mutex/src/main.rs
@@ -2,11 +2,10 @@ use std::sync::Mutex;
 
 fn main() {
     let my_mutex = Mutex::new(5);
-    let mut mutex_changer = my_mutex.lock().unwrap();
+    {
+        let mut mutex_changer = my_mutex.lock().unwrap();
+        *mutex_changer = 6;
+    }
 
     println!("{my_mutex:?}");
-    println!("{mutex_changer:?}");
-
-    *mutex_changer = 6;
-    println!("{mutex_changer:?}");
 }
