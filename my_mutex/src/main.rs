e7abb5b61c58040181bae93b8c47a3ba40b53168
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 11:34:03 +0700

    we can change the value right away

--- my_mutex/src/main.rs
+++ my_mutex/src/main.rs
@@ -3,16 +3,17 @@ use std::sync::Mutex;
 fn main() {
     let my_mutex = Mutex::new(5);
 
-    let mut mutex_changer = my_mutex.lock().unwrap();
+    // let mut mutex_changer = my_mutex.lock().unwrap();
     // *mutex_changer = 6;
     // drop(mutex_changer);
     //
     // println!("{my_mutex:?}");
-    let mut other_mutex_changer = my_mutex.try_lock();
-
-    if let Ok(value) = other_mutex_changer {
-        println!("The MutexGuard has: {value}")
-    } else {
-        println!("Didn't get the lock")
-    }
+    // let mut other_mutex_changer = my_mutex.try_lock();
+    //
+    // if let Ok(value) = other_mutex_changer {
+    //     println!("The MutexGuard has: {value}")
+    // } else {
+    //     println!("Didn't get the lock")
+    // }
+    *my_mutex.lock().unwrap() = 6;
 }
