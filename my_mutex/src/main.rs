50f187cea998b53ef9390f8d4dfb46d9f75d7a40
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 09:56:03 +0700

    understand the mutex changer structure and how to change value of a mutex

--- my_mutex/src/main.rs
+++ my_mutex/src/main.rs
@@ -1,3 +1,12 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    let my_mutex = Mutex::new(5);
+    let mut mutex_changer = my_mutex.lock().unwrap();
+
+    println!("{my_mutex:?}");
+    println!("{mutex_changer:?}");
+
+    *mutex_changer = 6;
+    println!("{mutex_changer:?}");
 }
